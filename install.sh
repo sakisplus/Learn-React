6312dc352b1d56019f108a195b956b3ffa03bb7b
Athanaaios Galanis <ath.galanis@gmail.com>
2023-06-29 12:54:54 +0000

    Handle sample db

--- install.sh
+++ install.sh
@@ -5,3 +5,6 @@ git clone https://github.com/sakisplus/Learn-React_app.git app
 
 echo "Clone Json Server in server folder"
 git clone https://github.com/sakisplus/Learn-React_server.git server
+
+echo "Create demo db"
+cp server/data/db.json.sample server/data/db.json 
