0c338955790ce773c3cbee875eb5bc3af505854c
Athanaaios Galanis <ath.galanis@gmail.com>
2023-06-29 09:13:12 +0000

    Update script

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Clone React App in app folder"
-git clone https://github.com/sakisplus/Learn-React_app.git
+git clone https://github.com/sakisplus/Learn-React_app.git app
 
 echo "Clone Json Server in server folder"
-git clone https://github.com/sakisplus/Learn-React_server.git
+git clone https://github.com/sakisplus/Learn-React_server.git server
